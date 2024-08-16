c9302965f90db2574debed1be5b6a538b81f78c9
Tomer Geva <gevatomer66@gmail.com>
2024-08-16 23:22:41 +0300

    added ModuloExpression

---
+++ ShticellEngine/src/engine/expression/api/impl/ModuloExpression.java
@@ -0,0 +1,24 @@
+package engine.expression.api.impl;
+
+import engine.expression.api.Expression;
+import engine.sheet.api.CellType;
+import engine.sheet.api.EffectiveValue;
+import engine.sheet.impl.EffectiveValueImpl;
+
+public class ModuloExpression implements Expression {
+    private Expression left;
+    private Expression right;
+    public ModuloExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+    @Override
+    public EffectiveValue eval() {
+        EffectiveValue leftValue = left.eval();
+        EffectiveValue rightValue = right.eval();
+        // do some checking... error handling...
+        int result = leftValue.extractValueWithExpectation(Integer.class) % rightValue.extractValueWithExpectation(Integer.class);
+
+        return new EffectiveValueImpl(CellType.NUMERIC, result);
+    }
+}
