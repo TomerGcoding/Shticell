aa4c2640a1fd47ab0f6009466f44e4b450bcfd58
Tomer Geva <gevatomer66@gmail.com>
2024-08-16 23:16:25 +0300

    added TimesExpression

---
+++ ShticellEngine/src/engine/expression/api/impl/TimesExpression.java
@@ -0,0 +1,25 @@
+package engine.expression.api.impl;
+
+import engine.expression.api.Expression;
+import engine.sheet.api.CellType;
+import engine.sheet.api.EffectiveValue;
+import engine.sheet.impl.EffectiveValueImpl;
+
+public class TimesExpression implements Expression {
+    private Expression left;
+    private Expression right;
+    public TimesExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+    @Override
+    public EffectiveValue eval() {
+        EffectiveValue leftValue = left.eval();
+        EffectiveValue rightValue = right.eval();
+        // do some checking... error handling...
+        double result = leftValue.extractValueWithExpectation(Double.class) * rightValue.extractValueWithExpectation(Double.class);
+
+        return new EffectiveValueImpl(CellType.NUMERIC, result);
+    }
+
+}
