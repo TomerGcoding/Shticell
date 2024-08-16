412971971a87c6815558398128973db500a7cce1
Tomer Geva <gevatomer66@gmail.com>
2024-08-17 00:20:11 +0300

    added some features on Sheet and SheetImpl

--- ShticellEngine/src/engine/sheet/api/Sheet.java
+++ ShticellEngine/src/engine/sheet/api/Sheet.java
@@ -1,10 +1,15 @@
 package engine.sheet.api;
 
 import engine.sheet.cell.api.Cell;
+import engine.sheet.impl.SheetImpl;
 
 public interface Sheet {
     int getVersion();
     Cell getCell(int row, int column);
     void setCell(int row, int column, String value);
-
+    String getSheetName();
+    int getRowCount();
+    int getColumnCount();
+    int getColumnWidth();
+    int getRowHeight();
 }
--- ShticellEngine/src/engine/sheet/impl/SheetImpl.java
+++ ShticellEngine/src/engine/sheet/impl/SheetImpl.java
@@ -1,7 +1,9 @@
 package engine.sheet.impl;
 
+import engine.sheet.api.EffectiveValue;
 import engine.sheet.api.Sheet;
 import engine.sheet.cell.api.Cell;
+import engine.sheet.cell.impl.CellImpl;
 import engine.sheet.coordinate.Coordinate;
 import engine.sheet.coordinate.CoordinateFactory;
 
@@ -11,15 +13,50 @@ import java.util.Map;
 public class SheetImpl implements Sheet {
 
     private Map<Coordinate, Cell> activeCells;
+    private static int currVersion = 0;
+    private String sheetName;
+    private SheetProperties properties;
 
-    public SheetImpl() {
+    public SheetImpl(String sheetName,int rows, int columns,int rowHeight, int columnWidth) {
         this.activeCells = new HashMap<>();
+        this.sheetName = sheetName;
+        properties = new SheetProperties(rows, columns, rowHeight, columnWidth);
+    }
+
+    @Override
+    public int getRowCount() {
+        return properties.numRows;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return properties.numCols;
+    }
+
+    @Override
+    public int getColumnWidth() {
+        return properties.colWidth;
+    }
+
+    @Override
+    public int getRowHeight() {
+        return properties.rowHeight;
+    }
+
+    @Override
+    public String getSheetName() {
+        return sheetName;
     }
 
     @Override
     public int getVersion() {
-        return 0;
+        return currVersion;
     }
+
+    public SheetProperties getSheetProperties(){
+        return properties;
+    }
+
 
     @Override
     public Cell getCell(int row, int column) {
@@ -30,8 +67,47 @@ public class SheetImpl implements Sheet {
     public void setCell(int row, int column, String value) {
         Coordinate coordinate = CoordinateFactory.createCoordinate(row, column);
         Cell cell = activeCells.get(coordinate);
+        if (cell == null) {
+            cell = new CellImpl(row,column,value,currVersion);
+            cell.calculateEffectiveValue();
+            activeCells.put(coordinate,cell);
+        }
         // if null need to create the cell...
+        else{
+            cell.setCellOriginalValue(value);
+            cell.calculateEffectiveValue();
+        }
 
-        cell.setCellOriginalValue(value);
+    }
+
+    private class SheetProperties {
+        private final int numCols;
+        private final int numRows;
+        private final int colWidth;
+        private final int rowHeight;
+
+        public SheetProperties(int numCols, int numRows, int colWidth, int rowHeight) {
+            this.numCols = numCols;
+            this.numRows = numRows;
+            this.colWidth = colWidth;
+            this.rowHeight = rowHeight;
+        }
+
+        public int getNumCols() {
+            return numCols;
+        }
+
+        public int getNumRows() {
+            return numRows;
+        }
+
+        public int getColWidth() {
+            return colWidth;
+        }
+
+        public int getRowHeight() {
+            return rowHeight;
+        }
+
     }
 }
