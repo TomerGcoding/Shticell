e269b97e027fb19287d781f733bb9e373bbb2e15
Tomer Geva <gevatomer66@gmail.com>
2024-08-17 14:25:00 +0300

    updated main for testing

--- ShticellUI/src/Main.java
+++ ShticellUI/src/Main.java
@@ -6,10 +6,10 @@ import ui.console.ConsoleUI;
 
 public class Main {
     public static void main(String[] args) {
-        Sheet sheet = new SheetImpl("First Sheet",4,4,4,4);
+        Sheet sheet = new SheetImpl("First Sheet",10,10,10,10);
         for (int i = 0; i < sheet.getProperties().getNumRows(); i++) {
             for (int j = 0; j < sheet.getProperties().getNumCols(); j++) {
-                sheet.setCell(i,j,"{SUB,TOMER,2.2,4.2}");
+                sheet.setCell(i,j,"{MINUS,9,{POW,2,3}}");
             }
         }
         Engine engine = new EngineImpl(sheet);  // Your engine implementation
