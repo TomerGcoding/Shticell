6f28b52f27ccd67bd5f6d29395901f435ed1a159
Danielle Shany <daniellesh2@mta.ac.il>
2024-10-13 13:22:26 +0300

    ex2 is working

--- ShticellEngine/src/main/java/com.shticell.engine/cell/api/EffectiveValue.java
+++
@@ -1,9 +0,0 @@
-package com.shticell.engine.cell.api;
-
-import com.shticell.engine.cell.impl.CellType;
-
-public interface EffectiveValue {
-    CellType getCellType();
-    Object getValue();
-    <T> T extractValueWithExpectation(Class<T> type);
-}
